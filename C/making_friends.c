5867eb910b5d789e93b8f5843294b9d557fc10aa
sanderz235 <fzk12321@gmail.com>
2026-04-01 23:27:20 +0800

    update

--- C/making_friends.c
+++ C/making_friends.c
@@ -1,70 +1,94 @@
-#include<stdio.h>
-#include<string.h>
-void my_strcpy(char*dest,char*src,int len)
-{
-	for(int i=0;i<len;i++)
-	{
-		dest[i]=src[i];
-	}
-	dest[len]='\0';
+#include <stdio.h>
+#include <string.h>
+#define MAX 105
+char name[MAX][15];
+int friend[MAX][MAX];
+int cnt[MAX];
+int total = 0;
+int find(char *s) {
+    for (int i = 0; i < total; i++)
+        if (strcmp(name[i], s) == 0)
+            return i;
+    strcpy(name[total], s);
+    return total++;
 }
-int main()
-{
-	int n,m;
-	scanf("%d %d",&n,&m);
-	getchar();
-	char name[101][11];
-	char relation[22],temp_name1[11],temp_name2[11];
-	int arr[101][101]={0};
-	int cnt=0;
-	for (int i = 0; i < m; i++)//m组关系
-	{
-		gets(relation);
-		int len=strlen(relation);
-		int len1=0,appear1=0,appear2=0;
-		int locate1=0,locate2=0;
-		while(relation[len1++]!=' ');
-		my_strcpy(temp_name1,relation,len1);
-		my_strcpy(temp_name2,relation+len1+1,len-len1-1);
-		for(int j=0;j<cnt;j++)
-		{
-			if(!strcmp(temp_name1,name[j]))
-			{
-				appear1=1;
-				locate1=j;
-			}
-			if(!strcmp(temp_name2,name[j]))
-			{
-				appear2=1;
-				locate2=j;
-			}
-		}
-		if(!appear1)
-		{
-			strcpy(name[cnt],temp_name1);
-			locate1=cnt;
-			cnt++;
-		}
-		if(!appear2)
-		{
-			strcpy(name[cnt],temp_name2);
-			locate2=cnt;
-			cnt++;
-		}
-		arr[locate1][locate2]=1;
-		arr[locate2][locate1]=1;
-	}
-	int max=0,num=0;
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<n;j++)
-		{
-			if(arr[i][j]==1)
-			num+=1;
-		}
-		if(num>max)max=num;
-		num=0;
-	}
-	printf("%d",max);
-	return 0;
-} 
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    memset(friend, 0, sizeof(friend));
+    memset(cnt, 0, sizeof(cnt));
+    for (int i = 0; i < m; i++) {
+        char a[15], b[15];
+        scanf("%s%s", a, b);
+        int x = find(a);
+        int y = find(b);
+        if (friend[x][y] == 0) {
+            friend[x][y] = 1;
+            friend[y][x] = 1;
+            cnt[x]++;
+            cnt[y]++;
+        }
+    }
+    int max = 0;
+    for (int i = 0; i < total; i++)
+        if (cnt[i] > max)
+            max = cnt[i];
+    printf("%d\n", max);
+    return 0;
+}
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     int n, m;
+//     scanf("%d %d", &n, &m);
+
+//     char name[101][11];
+//     int arr[101][101] = {0};
+//     int cnt = 0;
+
+//     for (int i = 0; i < m; i++) {
+//         char temp_name1[11], temp_name2[11];
+//         int appear1 = 0, appear2 = 0;
+//         int locate1 = 0, locate2 = 0;
+
+//         scanf("%s %s", temp_name1, temp_name2);
+
+//         for (int j = 0; j < cnt; j++) {
+//             if (strcmp(temp_name1, name[j]) == 0) {
+//                 appear1 = 1;
+//                 locate1 = j;
+//             }
+//             if (strcmp(temp_name2, name[j]) == 0) {
+//                 appear2 = 1;
+//                 locate2 = j;
+//             }
+//         }
+
+//         if (!appear1) {
+//             strcpy(name[cnt], temp_name1);
+//             locate1 = cnt;
+//             cnt++;
+//         }
+//         if (!appear2) {
+//             strcpy(name[cnt], temp_name2);
+//             locate2 = cnt;
+//             cnt++;
+//         }
+
+//         arr[locate1][locate2] = 1;
+//         arr[locate2][locate1] = 1;
+//     }
+
+//     int max = 0;
+//     for (int i = 0; i < cnt; i++) {
+//         int num = 0;
+//         for (int j = 0; j < cnt; j++) {
+//             if (arr[i][j] == 1) num++;
+//         }
+//         if (num > max) max = num;
+//     }
+
+//     printf("%d\n", max);
+//     return 0;
+// }
